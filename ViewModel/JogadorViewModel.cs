d7ffebe3f9453d558412fffd80d934afd2680040
PatrikRegis <78750688+PatrikRegis@users.noreply.github.com>
2021-11-25 20:15:16 -0300

    Add files via upload

---
+++ ViewModel/JogadorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace futebol2022.ViewModel
+{
+    [Serializable]
+    public class JogadorViewModel
+    {
+        [DisplayName("Nome do jogador")]
+        [Required(ErrorMessage = "Informe o nome do jogador")]
+        public string NomeJogador { get; set; }
+
+        [DisplayName("Apelido")]
+        public string Apelido { get; set; }
+       
+        [DisplayName("Foto do jogador")]
+        public IFormFile FotoJogador { get; set; }
+       
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:g}")]
+        [DataType(DataType.Date)]
+        public DateTime? DataNascimento { get; set; }
+    }
+}
